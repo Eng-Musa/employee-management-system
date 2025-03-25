1b3c475d5f80255601d8d20afafd9c264dfa8995
Musa <mangabomoses@gmail.com>
2025-03-25 15:01:34 +0300

    dashboard layout setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { DashboardLayoutComponent } from './components/dashboard-layout/dashboard-layout.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'login', pathMatch:'full'},
-    {path:'login', component:LoginComponent}
+    {path:'login', component:LoginComponent},
+    {path:'dashboard', component:DashboardLayoutComponent}
 ];
