0173b9f4dca7e3ce4e6ed1337fe85825058c9997
Musa <mangabomoses@gmail.com>
2025-03-26 11:47:54 +0300

    logout logic

--- src/app/components/dashboard-layout/dashboard-layout.component.ts
+++ src/app/components/dashboard-layout/dashboard-layout.component.ts
@@ -2,14 +2,29 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatFormField, MatInputModule, MatLabel } from '@angular/material/input';
+import {
+  MatFormField,
+  MatInputModule,
+  MatLabel,
+} from '@angular/material/input';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [MatIconModule, MatButtonModule, MatBadgeModule, MatFormField, MatLabel,MatInputModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatFormField,
+    MatLabel,
+    MatInputModule,
+    RouterModule,
+  ],
   templateUrl: './dashboard-layout.component.html',
-  styleUrl: './dashboard-layout.component.scss'
+  styleUrl: './dashboard-layout.component.scss',
 })
 export class DashboardLayoutComponent {
+  constructor(private router: Router) {}
 
+  logout() {this.router.navigate(['/login'])}
 }
