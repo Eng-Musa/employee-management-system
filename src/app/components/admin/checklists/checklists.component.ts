ba2149d3bab3c6a99c4a5191229fb780a9f5b434
Musa <mangabomoses@gmail.com>
2025-04-01 12:13:35 +0300

    checklists component template info

--- src/app/components/admin/checklists/checklists.component.ts
+++ src/app/components/admin/checklists/checklists.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-checklists',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './checklists.component.html',
-  styleUrl: './checklists.component.scss'
+  styleUrl: './checklists.component.scss',
 })
 export class ChecklistsComponent {
-
+  // Create an object with all your checklists under the "chskilist" key
+  checklistData = {
+    checklists: {
+      common: ['ID/passport', 'Birth certificate', 'Contact details'],
+      designer: [
+        'Submit design portfolio',
+        'Install Figma Suite',
+        'Submit signed NDA',
+        'Submit certificates',
+        'Technical Assessment',
+        'Share work tools',
+      ],
+      developer: [
+        'Share GitHub link',
+        'Submit software licenses',
+        'Submit certifications',
+        'Install dev tools',
+        'Technical Assessment',
+        'Access repositories',
+      ],
+      hr: [
+        'Submit HR certifications',
+        'Provide job references',
+        'Read company policies',
+        'Setup HR systems',
+        'Submit confidentiality form',
+      ],
+    },
+  };
 }
