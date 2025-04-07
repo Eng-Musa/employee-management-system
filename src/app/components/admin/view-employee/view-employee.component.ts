7300c3a8675dc034ec96013d07d0bf90c0ed8414
Musa <mangabomoses@gmail.com>
2025-04-07 09:26:12 +0300

    retrieve id and name from url

--- src/app/components/admin/view-employee/view-employee.component.ts
+++ src/app/components/admin/view-employee/view-employee.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-employee',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './view-employee.component.html',
   styleUrl: './view-employee.component.scss'
 })
-export class ViewEmployeeComponent {
+export class ViewEmployeeComponent implements OnInit {
+
+  employeeId!: number;
+  employeeName: string = '';
+
+  constructor(private route: ActivatedRoute){
+    
+  }
+  ngOnInit(): void {
+     // Retrieve the id from the route parameters
+     this.route.params.subscribe((params) => {
+      this.employeeId = +params['id']; // Convert to number
+      this.employeeName = params['name']; // Retrieve apartment name
+      console.log('Employee ID:', this.employeeId);
+      console.log('Employee Name:', this.employeeName);
+    });
+  }
 
 }
