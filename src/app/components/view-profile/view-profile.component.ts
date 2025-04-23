05e99afc2e786f6df3742f1c63d519ae43f21aa3
Musa <mangabomoses@gmail.com>
2025-04-23 16:34:31 +0300

    view profile ts refactor

--- src/app/components/view-profile/view-profile.component.ts
+++ src/app/components/view-profile/view-profile.component.ts
@@ -1,7 +1,8 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { constants } from '../../environments/constants';
 import { AlertService } from '../../services/alert.service';
 import { AuthService } from '../../services/auth.service';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 export interface LoggedInPerson {
   name: string;
@@ -33,7 +34,7 @@ export class ViewProfileComponent implements OnInit {
   };
 
   constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
+    private localStorageService: LocalStorageService,
     private alertService: AlertService,
     private authService: AuthService
   ) {}
@@ -43,49 +44,48 @@ export class ViewProfileComponent implements OnInit {
   }
 
   getLoggedInPerson() {
-    if (isPlatformBrowser(this.platformId)) {
-      if (this.authService.getUserType() === 'admin') {
-        const storedUserStr = localStorage.getItem('adminUser');
-        if (storedUserStr) {
-          this.loggedInPerson = JSON.parse(storedUserStr);
-        } else {
-          this.alertService.showErrorToastr(
-            'No admin user found in local storage.'
+    if (this.authService.getUserType() === 'admin') {
+      this.loggedInPerson = this.localStorageService.retrieve<any>(
+        constants.LOCAL_STORAGE_KEY_ADMIN
+      );
+      if (!this.loggedInPerson) {
+        this.alertService.showErrorToastr(
+          'No admin user found in local storage.'
+        );
+      }
+    } else {
+      const loggedInEmail = this.authService.getLoggedInEmail();
+      const employees = this.localStorageService.retrieve<any[]>(
+        constants.LOCAL_STORAGE_KEY_EMPLOYEES
+      );
+      if (employees) {
+        try {
+          const foundEmployee = employees.find(
+            (emp: any) => emp.email === loggedInEmail
           );
+          if (foundEmployee) {
+            this.loggedInPerson = {
+              name: foundEmployee.name,
+              email: foundEmployee.email,
+              password: foundEmployee.password,
+              createdDate: foundEmployee.createdDate,
+              role: foundEmployee.role,
+              phoneNumber: foundEmployee.phoneNumber,
+              lastLogin: foundEmployee.lastLogin,
+              lastPasswordChange: foundEmployee.lastPasswordChange,
+            };
+          } else {
+            this.alertService.showErrorToastr(
+              `Employee with email ${loggedInEmail} not found.`
+            );
+          }
+        } catch (error) {
+          this.alertService.showErrorToastr('Failed to parse employee data.');
         }
       } else {
-        const loggedInEmail = this.authService.getLoggedInEmail();
-        const employeesStr = localStorage.getItem('employees');
-        if (employeesStr) {
-          try {
-            const employees = JSON.parse(employeesStr);
-            const foundEmployee = employees.find(
-              (emp: any) => emp.email === loggedInEmail
-            );
-            if (foundEmployee) {
-              this.loggedInPerson = {
-                name: foundEmployee.name,
-                email: foundEmployee.email,
-                password: foundEmployee.password,
-                createdDate: foundEmployee.createdDate,
-                role: foundEmployee.role,
-                phoneNumber: foundEmployee.phoneNumber,
-                lastLogin: foundEmployee.lastLogin,
-                lastPasswordChange: foundEmployee.lastPasswordChange,
-              };
-            } else {
-              this.alertService.showErrorToastr(
-                `Employee with email ${loggedInEmail} not found.`
-              );
-            }
-          } catch (error) {
-            this.alertService.showErrorToastr('Failed to parse employee data.');
-          }
-        } else {
-          this.alertService.showErrorToastr(
-            'No employee data found in local storage.'
-          );
-        }
+        this.alertService.showErrorToastr(
+          'No employee data found in local storage.'
+        );
       }
     }
   }
