e8c523608a875a70e1f4531dc1ddd06ddf7a2052
Musa <mangabomoses@gmail.com>
2025-05-07 15:20:03 +0300

    should create login form with name and password controls

--- src/app/components/auth/login/login.component.spec.ts
+++ src/app/components/auth/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { LocalStorageService } from '../../../services/local-storage.service';
+import { FormGroup } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -26,4 +27,12 @@ describe('LoginComponent', () => {
   test('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  test.only('should create login form with name and password controls' , () =>{
+    const form = component.loginForm;
+    expect(form).toBeInstanceOf(FormGroup)
+    expect(form).toBeTruthy();
+    expect(form.contains('email')).toBeTruthy();
+    expect(form.contains('password')).toBeTruthy();
+  })
 });
