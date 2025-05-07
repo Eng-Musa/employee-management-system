afab492fb15fa68a3407d494d15c9695da6f7447
Musa <mangabomoses@gmail.com>
2025-05-07 11:31:33 +0300

    add employee tests to jest

--- src/app/components/admin/add-employee-dialogue/add-employee-dialogue.component.spec.ts
+++ src/app/components/admin/add-employee-dialogue/add-employee-dialogue.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddEmployeeDialogueComponent } from './add-employee-dialogue.component';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { constants } from '../../../environments/constants';
+import { LocalStorageService } from '../../../services/local-storage.service';
 
 describe('AddEmployeeDialogueComponent', () => {
   let component: AddEmployeeDialogueComponent;
@@ -8,16 +12,30 @@ describe('AddEmployeeDialogueComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddEmployeeDialogueComponent]
-    })
-    .compileComponents();
+      imports: [AddEmployeeDialogueComponent],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+        {
+          provide: LocalStorageService,
+          useValue: {
+            retrieve: jest
+              .fn()
+              .mockReturnValue(constants.LOCAL_STORAGE_KEY_EMPLOYEES),
+          },
+        },
+        { provide: MatSnackBar, useValue: { open: jest.fn() } },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AddEmployeeDialogueComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
 });
