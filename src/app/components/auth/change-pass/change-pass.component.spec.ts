551ac0c4d574624edddecc9a95ca94b827c70ac5
Musa <mangabomoses@gmail.com>
2025-05-07 13:08:30 +0300

    change pass component tests error fiexes

--- src/app/components/auth/change-pass/change-pass.component.spec.ts
+++ src/app/components/auth/change-pass/change-pass.component.spec.ts
@@ -3,6 +3,7 @@ import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ChangePassComponent } from './change-pass.component';
+import { LocalStorageService } from '../../../services/local-storage.service';
 
 describe('ChangePassComponent', () => {
   let component: ChangePassComponent;
@@ -11,7 +12,10 @@ describe('ChangePassComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ChangePassComponent, MatDialogModule, BrowserAnimationsModule],
-      providers: [{ provide: MatDialogRef, useValue: {} }],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: LocalStorageService, useValue: { retrieve: jest.fn() } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ChangePassComponent);
