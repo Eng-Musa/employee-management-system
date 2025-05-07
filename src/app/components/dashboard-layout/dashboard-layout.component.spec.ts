82e13e9d14d8f8f00b92c051db5a8e7b6ea80ab2
Musa <mangabomoses@gmail.com>
2025-05-07 09:59:07 +0300

    dashboard layout tests to jest

--- src/app/components/dashboard-layout/dashboard-layout.component.spec.ts
+++ src/app/components/dashboard-layout/dashboard-layout.component.spec.ts
@@ -17,7 +17,7 @@ describe('DashboardLayoutComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
 });
