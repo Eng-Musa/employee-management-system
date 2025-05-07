4be849d9ccae93c3143ef9976a5a4c82c422d2b0
Musa <mangabomoses@gmail.com>
2025-05-07 15:41:22 +0300

    should make the email required test

--- src/app/components/auth/login/login.component.spec.ts
+++ src/app/components/auth/login/login.component.spec.ts
@@ -28,11 +28,20 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  test.only('should create login form with name and password controls' , () =>{
+  test('should create login form with name and password controls' , () =>{
     const form = component.loginForm;
     expect(form).toBeInstanceOf(FormGroup)
     expect(form).toBeTruthy();
     expect(form.contains('email')).toBeTruthy();
     expect(form.contains('password')).toBeTruthy();
+  });
+
+  test.only('should make the email required', () =>{
+    const control = component.loginForm.get('email');
+    expect(control).toBeTruthy();
+    control!.setValue('');
+    expect(control?.valid).toBeFalsy();
+    expect(control?.hasError('required')).toBeTruthy();
   })
+
 });
