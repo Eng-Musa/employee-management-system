ccac3961eb21f4935cf8ea67f9c8393132623aa2
Musa <mangabomoses@gmail.com>
2025-05-08 15:10:41 +0300

    setting change pass for testing

--- src/app/components/auth/change-pass/change-pass.component.spec.ts
+++ src/app/components/auth/change-pass/change-pass.component.spec.ts
@@ -4,17 +4,37 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ChangePassComponent } from './change-pass.component';
 import { LocalStorageService } from '../../../services/local-storage.service';
+import { AlertService } from '../../../services/alert.service';
+import { AuthService } from '../../../services/auth.service';
+import { FormBuilder } from '@angular/forms';
 
 describe('ChangePassComponent', () => {
   let component: ChangePassComponent;
   let fixture: ComponentFixture<ChangePassComponent>;
+  let mockDialogRef: Partial<MatDialogRef<ChangePassComponent>>;
+  let mockAlert: Partial<AlertService>;
+  let mockAuth: Partial<AuthService>;
+  let mockLS: Partial<LocalStorageService>;
 
   beforeEach(async () => {
+    jest.useFakeTimers();
+
+    mockDialogRef = { close: jest.fn() };
+    mockAlert = { error: jest.fn(), success: jest.fn() };
+    mockAuth = {
+      getUserType: jest.fn().mockReturnValue('admin'),
+      logout: jest.fn(),
+    };
+    mockLS = { retrieve: jest.fn(), save: jest.fn() };
+
     await TestBed.configureTestingModule({
       imports: [ChangePassComponent, MatDialogModule, BrowserAnimationsModule],
       providers: [
-        { provide: MatDialogRef, useValue: {} },
-        { provide: LocalStorageService, useValue: { retrieve: jest.fn() } },
+        FormBuilder,
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: AlertService, useValue: mockAlert },
+        { provide: AuthService, useValue: mockAuth },
+        { provide: LocalStorageService, useValue: mockLS },
       ],
     }).compileComponents();
 
