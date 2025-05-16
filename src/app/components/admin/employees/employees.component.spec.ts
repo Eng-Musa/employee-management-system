29dfc38aea67ee7bedfd0c03267d60cf92d52b95
Musa <mangabomoses@gmail.com>
2025-05-16 11:04:46 +0300

    should call fetchEmployees on initialization test

--- src/app/components/admin/employees/employees.component.spec.ts
+++ src/app/components/admin/employees/employees.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeesComponent } from './employees.component';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../../services/local-storage.service';
 
 describe('EmployeesComponent', () => {
   let component: EmployeesComponent;
   let fixture: ComponentFixture<EmployeesComponent>;
+  let dialogMock: Partial<MatDialog>;
+  let routerMock: Partial<Router>;
+  let localStorageServiceMock: Partial<LocalStorageService>;
 
   beforeEach(async () => {
+    dialogMock = { open: jest.fn() };
+    routerMock = { navigate: jest.fn() };
+    localStorageServiceMock = { retrieve: jest.fn().mockReturnValue([]) };
+
     await TestBed.configureTestingModule({
-      imports: [EmployeesComponent]
+      imports: [EmployeesComponent],
+       providers: [
+        { provide: MatDialog, useValue: dialogMock },
+        { provide: Router, useValue: routerMock },
+        { provide: LocalStorageService, useValue: localStorageServiceMock }
+      ]
     })
     .compileComponents();
 
@@ -19,5 +34,11 @@ describe('EmployeesComponent', () => {
 
   test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should call fetchEmployees on initialization', () => {
+    const fetchSpy = jest.spyOn(component, 'fetchEmployees');
+    component.ngOnInit();
+    expect(fetchSpy).toHaveBeenCalled();
   });
 });
